f6280566df00f94a4db7fb89513a5ba290cdb138
Andrey Evstifeev <andrey.evstifeev@gmail.com>
2018-02-09 18:04:39 +0300

    Angular1/conFusion - week1 - wip

--- Angular1/conFusion/src/app/app.module.ts
+++ Angular1/conFusion/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
 
