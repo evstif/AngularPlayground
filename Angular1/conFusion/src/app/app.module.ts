636bfd230430221b3af20d97ed34449c4f9744e2
Andrey Evstifeev <andrey.evstifeev@gmail.com>
2018-02-10 19:10:14 +0300

    Angular1/conFusion configuration

--- Angular1/conFusion/src/app/app.module.ts
+++ Angular1/conFusion/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material'
+import { MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +16,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
